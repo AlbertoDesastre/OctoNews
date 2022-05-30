54132957dd47c23f65107090f4b1bd4a25957ef3
Eduardo Collazo <ecollazodominguez@gmail.com>
2022-05-30 19:28:28 +0200

    Carousel and newsCard component upgraded

--- src/components/NewsFeedList.js
+++ src/components/NewsFeedList.js
@@ -3,32 +3,31 @@ import { get } from "../utils/api";
 import { NewsCards } from "./NewsCards";
 import "./NewsFeedList.css";
 
-export const NewsFeedList = () => {
+export const NewsFeedList = ({ categories }) => {
   const [newsList, setNewsList] = useGetRemoteData(
-    `${process.env.REACT_APP_BACKEND}/news?date=2022-04-29`,
+    `${process.env.REACT_APP_BACKEND}/news`,
     get
   );
 
   return (
     <section className="feed">
       <ul>
-        {newsList?.news ? (
-          newsList.news.map((news) => {
-            return (
-              <li key={news.id} className="news">
-                <NewsCards
-                  title={news.title}
-                  image={news.image}
-                  description={news.introduction_text}
-                  text={news.news_text}
-                  votes={news.votos}
-                />
-              </li>
-            );
-          })
-        ) : (
-          <p>loading...</p>
-        )}
+        {newsList.map((news) => {
+          return (
+            <li key={news.id} className="news">
+              <NewsCards
+                username={news.user_name}
+                date={news.creation_date}
+                title={news.title}
+                image={news.image}
+                description={news.introduction_text}
+                text={news.news_text}
+                votes={news.votos}
+                category={categories.find((e) => e.id === news.id_category)}
+              />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
