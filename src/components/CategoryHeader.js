ac7405590b44d9d2c68d055e0299376bd89e38ca
Eduardo Collazo <ecollazodominguez@gmail.com>
2022-06-06 16:51:04 +0200

    separated JSX structure from Category page into component

---
+++ src/components/CategoryHeader.js
@@ -0,0 +1,16 @@
+import "./CategoryHeader.css";
+
+export const CategoryHeader = () => {
+  return (
+    <section className="category-page-header">
+      <header className="category-page-header">
+        <img src="/categoryIcons/2134234234252.jpg" alt="category header" />
+      </header>
+      <article className="category-page">
+        <img src="/categoryIcons/2134234234252.jpg" alt="category" />
+        <h2>Category</h2>
+        <p>Todas las noticias que puedes esperar con un toque 'pintoresco'</p>
+      </article>
+    </section>
+  );
+};
