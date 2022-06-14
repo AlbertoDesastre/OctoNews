d4fb3cea30922b849387129fadcbf265ae86db01
carmenpelaez <carmenpelaezcrespo@gmail.com>
2022-06-14 16:49:33 +0200

    css login and user create

--- src/pages/UserLogin.js
+++ src/pages/UserLogin.js
@@ -2,7 +2,8 @@ import { useContext, useState } from "react";
 import { Header } from "../components/Header";
 import { AuthContext } from "../context/AuthContext";
 import { loginUserService } from "../utils/api";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import "./userAuth.css";
 
 export const LoginPage = () => {
   const { token, login } = useContext(AuthContext);
@@ -28,38 +29,39 @@ export const LoginPage = () => {
   return (
     <>
       <Header />
-      <main>
-        <section>
-          <h1>Login</h1>
-          <form>
-            <fieldset>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </fieldset>
+      <main className="auth">
+        <h1>Login</h1>
+        <form class="auth">
+          <fieldset className="email">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
 
-            <fieldset>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </fieldset>
+          <fieldset className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
 
-            <button type="submit" onClick={handleForm}>
-              Login
-            </button>
-            {error ? <p>{error}</p> : null}
-          </form>
-        </section>
+          <button type="submit" onClick={handleForm}>
+            Login
+          </button>
+          {error ? <p>{error}</p> : null}
+        </form>
+        <p>
+          Fist time on OctoNews? <Link to="/users/create"> Register </Link>
+        </p>
       </main>
     </>
   );
